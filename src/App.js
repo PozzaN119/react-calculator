47baec8c8b9a5fe6cfd29f9f816c198901c26c58
NPozza119 <noe.pozza@gmail.com>
2022-06-27 11:51:44 +0200

    react-calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import React, { useState } from 'react'
 import Key from './key.js'
 
+const style = require('./style.json');
+
 function App() {
 
-  const [count, setCount] = useState(0)
-  /*let displayVal = "$null"*/
+  const [EQ, setEQ] = useState(() => {
+    let init = {
+      "nbr1":0,
+      "op":0,
+      "nbr2":0
+    }
+    return init
+  })
+
+  const clickHandler = (elEQ, val, e, debug = false) => {
+    let copieEQ = {...EQ}
+    const r = new Promise( _ => {})
+    if(elEQ.constructor === Array && val.constructor === Array) {
+      if(!(elEQ.length === val.length)) return r
+
+      for(let i=0; i< elEQ.length; i++) {
+        copieEQ[elEQ[i]] = val[i]
+      }
+    } else if(elEQ.constructor === String && val.constructor === Number) {
+      copieEQ[elEQ] = val
+    }
+
+    setEQ(copieEQ)
+    if(debug) console.log(EQ)
+  }
+
   return (
-    <div>
-      <h1>Calculator</h1>
-      <Interface val={count}/>
-      <Key val="test" s={setCount} c={count}/>
+    <div style={style.Main}>
+      <Interface val={EQ}/>
+      <Key val="7" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="8" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="9" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="+" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="4" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="5" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="6" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="-" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="1" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="2" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="3" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="*" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="<" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="0" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="=" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
+      <Key val="/" sEQ={setEQ} EQ={EQ} handler={clickHandler}/>
     </div>
   )
 }
 
 function Interface(props) {
+  let displayVal = props.val.nbr2
+  if(props.val.nbr2 === 0) displayVal = props.val.nbr1
+
   return (
-    <div>
-      <input value={props.val} disabled/>
+    <div style={style.Header}>
+      <p style={style.Title}>Calculator</p>
+      <input placeholder='$null' value={displayVal} style={style.Interface}  disabled/>
     </div>
   )
 }
--- src/key.js
+++ src/key.js
@@ -1,11 +1,109 @@
 import React from 'react'
 
+const style = require('./style.json');
+
+function getOpId(op) {
+  let r = null
+
+  if(op === "+") r = 1
+  if(op === "-") r = 2
+  if(op === "*") r = 3
+  if(op === "/") r = 4
+
+  return r
+}
+
+function fusionTwoNbr(nbrBase, nbrToAdd) {
+
+  let lenShift = String(nbrToAdd).length
+  let r = nbrBase * (10**lenShift)
+  r = r + nbrToAdd
+
+  return r
+}
+
+function deleteLastDigitOfNbr(nbr) {
+  let r = String(nbr)
+  console.log(r.length)
+  if(r.length > 1) {
+    r = r.slice(0,-1)
+  } else {
+    r = 0
+  }
+  return parseInt(r)
+}
+
+function exeOp(nbr1, nbr2, op) {
+
+  return new Promise( res => {
+    if(op === 1) {
+      res(exeSum(nbr1, nbr2))
+    } if(op === 2) {
+      res(exeMin(nbr1, nbr2))
+    } if(op === 3) {
+      res(exeTim(nbr1, nbr2))
+    } if(op === 4) {
+      res(Math.round(exeDiv(nbr1, nbr2)))
+    } 
+  })
+}
+
+function exeSum(nbr1, nbr2) {return (parseInt(nbr1) + parseInt(nbr2))}
+function exeMin(nbr1, nbr2) {return (parseInt(nbr1) - parseInt(nbr2))}
+function exeTim(nbr1, nbr2) {return (parseInt(nbr1) * parseInt(nbr2))}
+function exeDiv(nbr1, nbr2) {return (parseInt(nbr1) / parseInt(nbr2))}
+
+function fromCharToNumber(c) {
+  let l = ['+','-','*','/','<','=']
+  if(l.includes(c)) return(l.indexOf(c) + 10)
+  return c
+}
+
 function key(props) {
+
+  const s = {
+    "grid-area": "key"+fromCharToNumber(props.val)
+  }
+
+  // console.log(s)
+
+  const click = async(e) => {
+
+    // props.handler("nbr1", parseInt(props.val), e, true)
+
+    // Enter case
+    if([">","="].includes(props.val)) {
+      const res = await exeOp(props.EQ.nbr1, props.EQ.nbr2, props.EQ.op)
+      console.log(res)
+      await props.handler(["nbr1","nbr2","op"], [res,0,0], e, true)
+
+    // Delete case
+    } else if(props.val === "<") {
+      if(props.EQ.op === 0) {
+        props.handler("nbr1", deleteLastDigitOfNbr(props.EQ.nbr1), e, true)
+      } else {
+        props.handler("nbr2", deleteLastDigitOfNbr(props.EQ.nbr2), e, true)
+      }
+    
+    // Operator key on case
+    } else if(["+","-","*","/"].includes(props.val)){
+      props.handler("op", getOpId(props.val), e, true)
+    
+    // Number key on case
+    } else {
+      if(props.EQ.op === 0) {
+        props.handler("nbr1", fusionTwoNbr(props.EQ.nbr1, parseInt(props.val)), e, true)
+      } else {
+        props.handler("nbr2", fusionTwoNbr(props.EQ.nbr2, parseInt(props.val)), e, true)
+      }
+    }
+  }
+  
   return (
-    <div>
-        <button onClick={() => props.s(props.c+1) }>
-            {props.val}
-        </button>
+    <div style={s}>
+      <button onClick= {click} style={style.Key}>
+        {props.val}
+      </button>
     </div>
   )
 }
